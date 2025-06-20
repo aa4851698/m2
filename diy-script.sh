70bb0c98a08f83bb1713f7536fbecb3b60e5f27f
aa4851698 <sgwapk@163.com>
2025-06-20 08:14:24 -0700

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -78,12 +78,4 @@ find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/PKG_SOURCE_U
 
 
 ./scripts/feeds update -a
-
-# 修改 golang
-
-rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
-rm -rf feeds/packages/utils/v2dat
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
-
 ./scripts/feeds install -a
